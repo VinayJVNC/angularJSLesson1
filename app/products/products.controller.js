39da82949bee90f7366f529bd02dc892415bf258
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2017-04-11 09:36:01 +0530

    Udate product functionalities

--- app/products/products.controller.js
+++ app/products/products.controller.js
@@ -32,7 +32,7 @@ app.controller('productsController',function($scope,$mdDialog,$mdToast,productsF
 	//Create New Product
 	$scope.createProducts = function(){
 		productsFactory.createProducts($scope).then(function successCallBack(response){
-			$scope.showToast(response.data); //tell the use new product is created
+			$scope.showToast(response.data.message); //tell the use new product is created
 			$scope.readProducts(); //refresh product list
 			$scope.clearProductForm(); //Clear the form values 			
 		},function errorCallback(response){
@@ -46,7 +46,7 @@ app.controller('productsController',function($scope,$mdDialog,$mdToast,productsF
 		$scope.description = "";
 		$scope.price = "";
 	}
-	$scope.showToast = function(){
+	$scope.showToast = function(message){
 		$mdToast.show(
 			$mdToast.simple()
 			.textContent(message)
@@ -58,10 +58,10 @@ app.controller('productsController',function($scope,$mdDialog,$mdToast,productsF
 	//read single product record
 
 	$scope.readSingleRecord = function(id){
-		debugger;
+		//debugger;
 		productsFactory.readSingleProduct(id).then(function successCallback(response){
 			$scope.id = response.data.id;
-			debugger;
+			//debugger;
 			$scope.name = response.data.name;
 			$scope.description = response.data.description;
 			$scope.price = response.data.price;
@@ -77,7 +77,6 @@ app.controller('productsController',function($scope,$mdDialog,$mdToast,productsF
 				fullscreen : true
 			}).then(
 				function(){},
-
 				function(){
 					$scope.clearProductForm();
 				}
@@ -87,6 +86,71 @@ app.controller('productsController',function($scope,$mdDialog,$mdToast,productsF
 			$scope.showToast("Unable to retrieve record.");
 		});
 	}
+	
+	//Retrieve data to update form
 
-	//Update Product form
+	$scope.showProductUpdateForm = function(id){
+		productsFactory.readSingleProduct(id).then(function successCallback(response){			
+			$scope.id = response.data.id;
+			$scope.name = response.data.name;
+			$scope.description = response.data.description;
+			$scope.price = response.data.price;
+console.log(response);
+			$mdDialog.show({
+				controller:DialogController,
+				templateUrl : './app/products/update_products.template.html',
+				parent: angular.element(document.body),
+				targetEvent : event,
+				clickOutsideToClose: true,
+				scope: $scope,
+				preserveScope: true,
+				fullscreen: true
+			}).then(
+				function(){},
+				function(){
+					$scope.clearProductForm();
+				}
+			);
+		},function errorCallback(response){
+			$scope.showToast("Unable to retrieve data.");
+		});
+	}
+
+	//Update Form
+	$scope.updateProduct = function(){		
+		productsFactory.updateProduct($scope).then(function successCallback(response){			
+			$scope.showToast(response.data.message);
+			$scope.readProducts();
+			$scope.cancel();
+			$scope.clearProductForm();
+		},function errorCallback(response){
+			$scope.showToast("Unable to update record");
+		});
+	}
+	$scope.deleteProductController = function(event,id){
+		$scope.id = id;
+		var confirm = $mdDialog.confirm()
+			.title('Are you sure to delete ?')
+			.textContent('Product will be deleted')
+			.targetEvent(event)
+			.ok('Yes')
+			.cancel('No');
+		$mdDialog.show(confirm).then(
+			//If yes delete product
+			function(){
+				$scope.deleteProduct();
+			},
+			//If not hide dialog box
+			function(){
+
+			});
+	}
+	$scope.deleteProduct = function(){
+		productsFactory.deleteProductFactory($scope.id).then(function successCallback(response){
+			$scope.showToast(response.data.message);
+			$scope.readProducts();
+		},function errorCallback(){
+			$scope.showToast("Unable to Delete Product");
+		});
+	}
 });
--- app/products/products.factory.js
+++ app/products/products.factory.js
@@ -29,6 +29,33 @@ app.factory("productsFactory",function($http){
 			url : 'http://localhost/codeofaninja_api/product/readSinglerecord.php?id='+id
 		});
 	};
+	
+	//Update  Product
+	factory.updateProduct = function($scope){
 
+		return $http({
+			method: "POST",
+			data : {
+				'id':$scope.id,
+				'name' : $scope.name,
+				'description' : $scope.description,
+				'price' : $scope.price,
+				'category_id' : 1
+			},
+			url: 'http://localhost/codeofaninja_api/product/updateRecord.php'
+		});
+	}
+
+	//delete products
+	factory.deleteProductFactory = function(id){
+		console.log("deleteProductFactory id: " + id);
+		return $http({
+			method : 'POST',
+			data : {
+				'id':id,
+			},
+			url : 'http://localhost/codeofaninja_api/product/deleteRecord.php'
+		});
+	}
 	return factory;
 });
